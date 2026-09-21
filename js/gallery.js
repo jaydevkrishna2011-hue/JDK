6705fc99fbb92197b7134c669d9a5ac6a2186d4c
Jaydev Krishna <jaydevkrishna2011@gmail.com>
2026-09-21 19:00:08 +0530

    Fix gallery mobile menu toggle

--- js/gallery.js
+++ js/gallery.js
@@ -1681,7 +1681,7 @@ document.addEventListener("DOMContentLoaded", () => {
       return;
     }
 
-    mobileMenu.classList.add("is-active");
+    mobileMenu.classList.add("is-open");
 
     mobileMenu.setAttribute("aria-hidden", "false");
 
@@ -1693,7 +1693,7 @@ document.addEventListener("DOMContentLoaded", () => {
       return;
     }
 
-    mobileMenu.classList.remove("is-active");
+    mobileMenu.classList.remove("is-open");
 
     mobileMenu.setAttribute("aria-hidden", "true");
 
